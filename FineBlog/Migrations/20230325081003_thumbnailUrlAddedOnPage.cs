7c3d45671f96cab40ff072477ac5d94d466bc06e
nvnin <1612.nvn@gmail.com>
2023-03-25 21:23:07 +0700

    Add project files.

---
+++ FineBlog/Migrations/20230325081003_thumbnailUrlAddedOnPage.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FineBlog.Migrations
+{
+    public partial class thumbnailUrlAddedOnPage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CreatedDAte",
+                table: "Posts",
+                newName: "CreatedDate");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ThumbnailUrl",
+                table: "Pages",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ThumbnailUrl",
+                table: "Pages");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedDate",
+                table: "Posts",
+                newName: "CreatedDAte");
+        }
+    }
+}
